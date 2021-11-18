a6421824520b937d7e2cffeb71515db3f1c6d964
David Yappeter <davidyap11les@gmail.com>
2021-11-18 23:28:50 +0700

    Initialize Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config({ path: __dirname + "/.env.local" });
+// require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(require("./routes/index"));
+
+app.listen(PORT, () => {
+  console.log(`Listen and serve at http://localhost:${PORT}`);
+});
