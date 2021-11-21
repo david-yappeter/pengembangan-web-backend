397b3c17cdc88fd796be549a102ba8c4f477bd20
David Yappeter <davidyap11les@gmail.com>
2021-11-21 23:30:14 +0700

    latest update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: __dirname + "/.env.local" });
 // require("dotenv").config();
 // const sequelize = require("./db/sequelize");
@@ -8,10 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.use(require("./routes/index"));
+app.use(require("./src/routes/apis/login"));
+// app.use(require("./src/routes/web/login"));
 
 app.listen(PORT, () => {
   console.log(`Listen and serve at http://localhost:${PORT}`);
---
+++ src/routes/apis/login.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { userInfo } = require("os");
+const passport = require("passport");
+const CookieStrategy = require("passport-cookie");
+
+const router = express.Router();
+// passport.use(
+//   new CookieStrategy(function (token, done) {
+//     done(null, { id: "123", username: "test" });
+//   })
+// );
+passport.use(
+  new CookieStrategy(function (token, done) {
+    done(null, { id: "123", username: "test" });
+  })
+);
+
+router.get(
+  "/api/users/profile",
+  passport.authenticate("cookie", { session: false }),
+  function (req, res) {
+    res.json(req);
+  }
+);
+
+router.post("/api/users/login", (req, res) => {
+  res.cookie(
+    "token",
+    {
+      val: "of cookie",
+    },
+    {
+      httpOnly: true,
+    }
+  );
+
+  return res.status(200).send();
+});
+
+module.exports = router;
